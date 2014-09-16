b1667ad117538e39d738f5fe8db80b2e9b5d9436
Zelious <sigmazelious@gmail.com>
2014-09-16 15:27:14 -0400

    First attempt to get rid of meta files
    Don't think I need them as reference and get in the way when I expand
    all.

---
+++ Assets/Scripts/CoroutineEnemySpawnManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CoroutineEnemySpawnManager : MonoBehaviour {
+
+	[SerializeField]
+	GameObject playerToSpawn;
+	[SerializeField]
+	float delayTime = 5f;
+
+
+	void Start () 
+	{
+		StartCoroutine(EnemySpawn());
+	}
+	
+	IEnumerator EnemySpawn()
+	{
+		while(true)
+		{
+			Instantiate(playerToSpawn, transform.position, Quaternion.identity);
+			yield return new WaitForSeconds(delayTime);
+		}
+
+	}
+
+}
---
+++ Assets/Scripts/EnemySpawnManager.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemySpawnManager : MonoBehaviour {
+
+	[SerializeField]
+	GameObject enemyToSpawn;
+	[SerializeField]
+	float delaySpawn;
+
+	float canSpawn;
+
+	void Start () 
+	{
+		delaySpawn = 5.0f;
+		canSpawn = -1;
+	}
+	
+
+	void Update () 
+	{
+		if(Time.time >= canSpawn)
+		{
+			Instantiate(enemyToSpawn);
+			canSpawn = Time.time + delaySpawn;
+		}
+
+	}
+	
+}
